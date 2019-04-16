6132a00236e8fde8c205315bcba352ef2065c363
huang-j <jonayboy@gmail.com>
2019-04-16 18:03:22 -0500

    changed input and output

--- crCounts.sh
+++ crCounts.sh
@@ -1,50 +1,34 @@
 #!/bin/bash
 ## Run Collect read counts on multiple samples
 usage() {
-    echo "Usage: $0 [-d <string>] [-l <string>] [-b <string>] [-i <string>] [-e <string>]"
-    echo "        -d location directory"
-    echo "        -l file containing names of samples to be used"
-    echo "                e.g. MK29-T_S4"
-    echo "        -b bam qualifier, (sorted, gc_corrected etc.)"
-    echo "                e.g. MK29-T_S4.sorted.bam"
+    echo "Usage: $0 [-f <string>] [-i <string>] [-e <string>] [-o <string>]"
+    echo "        -f file"
     echo "        -i preprocessed interval list"
     echo "        -e extra stuff to name"
+    echo "        -o output"
     exit 1;
 }
-while getopts ":d:l:b:i:e:" o; do
-    case "${o}" in
-        d)
-            d=${OPTARG}
-            ;;
-        l)
-            l=${OPTARG}
-            ;;
-        b)
-            b=${OPTARG}
+while getopts ":f:i:e:o:" x; do
+    case "${x}" in
+        f)
+            f=${OPTARG}
             ;;
         i)
             i=${OPTARG}
             ;;
-
         e)
             e=${OPTARG}
             ;;
-
-
+        o)
+            o=${OPTARG}
+            ;;
         *)
             usage
             ;;
     esac
 done
-if [ -z "${l}" ]; then
+if [ -z "${f}" ]; then
     usage
-fi
-if [ -z "${d}" ]; then
-    d="Agilent/firstSet"
-fi
-if [ -z "${b}" ]; then
-    usage   
-#  b="sorted"
 fi
 if [ -z "${i}" ]; then
     usage
@@ -52,12 +36,14 @@ fi
 if [ -z ${e} ]; then
     e=""
 fi
-for s in $(cat ${l}); do
-    gatk CollectReadCounts \
-        -I ${d}/Bams/${s}/${s}.${b}.bam \
-        -L ${i} \
-        --interval-merging-rule OVERLAPPING_ONLY \
-        --read-filter MappingQualityReadFilter --minimum-mapping-quality 10 \
-        -DF NotDuplicateReadFilter \
-        -O CNV/${s}${e}.counts.hdf5
-done
+if [ -z ${o} ]; then
+    o="${f%.bam}.counts.hdf5"
+fi
+s=
+gatk CollectReadCounts \
+    -I ${f} \
+    -L ${i} \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    --read-filter MappingQualityReadFilter --minimum-mapping-quality 10 \
+    -DF NotDuplicateReadFilter \
+    -O ${o}
